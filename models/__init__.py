c65f908f3a9a09abe697927ab93e050114521edb
Caio Herrera <caioherrera36@gmail.com>
2025-11-02 16:49:42 -0300

    feat: created tests

--- models/__init__.py
+++
@@ -1,6 +0,0 @@
-from .users.users import User
-from .products.products import Product
-from .orders.orders import Order
-from .orders.order_items import OrderItem
-
-__all__ = ["User", "Product", "Order", "OrderItem"]
